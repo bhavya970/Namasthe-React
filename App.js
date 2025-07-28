216e364ae925ff8b1ce3060eb252349e720bd945
bhavya sree <bhavyasrigurram4@gmail.com>
2025-07-28 11:28:59 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,6 @@
+const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child",},[React.createElement("h1",{},"I'm H1"),React.createElement("h1",{},"I'm H2")]));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+// const heading = React.createElement("h1",{id:"heading",abc:"xyz "},"Hello World from React");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
