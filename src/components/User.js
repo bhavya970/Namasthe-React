80186175623d14a81d74f4b26fb394bf3186875e
bhavya sree <bhavyasrigurram4@gmail.com>
2025-12-14 20:02:52 +0530

    StateVariables

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,11 @@
+import { useState } from "react";
+
 const User = ({ name }) => {
+    const [count] = useState(0);
+    const [count2] = useState(1);
     return <div className="user-card">
+        <h1>Count = {count}</h1>
+        <h1>Count2 = {count2}</h1>
         <h2>Name:{name}</h2>
         <h2>Location:PapayaPalem</h2>
         <h3>Contact:bhavyanov5@</h3>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,10 +3,18 @@ import React from "react";
 class UserClass extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            count : 0,
+            count2 : 1
+        };
     }
     render() {
         const {name , location} = this.props;   
+        const {count , count2} = this.state;
         return <div className="user-card">
+        <h1>Count = {count}</h1>
+        <h1>Count2 = {count2}</h1>
         <h2>Name:{name}</h2>
         <h2>Location:{location}</h2>
         <h3>Contact:bhavyanov5@</h3>
