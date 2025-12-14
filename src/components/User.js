3bbd3487375981ff12a4718ee60f330edf1c459e
bhavya sree <bhavyasrigurram4@gmail.com>
2025-12-15 01:27:33 +0530

    LifeCycle of React ClassBased Components + clearing Messy in useEffect

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const User = ({ name }) => {
     const [count , setCount] = useState(0);
     const [count2] = useState(1);
+    useEffect( () => {
+        //API CALL
+        console.log("useEffect called");
+        const timer = setInterval( () => {
+            console.log("Timer");
+        }, 1000);
+        return () => {
+            clearInterval(timer);
+        }
+    }, []); //dependency array
+    console.log("Rendering User Component");
     return <div className="user-card">
         <h1>Count = {count}</h1>
         <h1>Count2 = {count2}</h1>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,12 +6,42 @@ class UserClass extends React.Component {
 
         this.state = {
             count : 0,
-            count2 : 1
+            count2 : 1,
+            userInfo : {
+                name : "Dummy Name",
+                location : "Dummy Location"
+            },
+            }
         };
+    async componentDidMount() {
+        this.timer =  setInterval( () => {
+            console.log("Timer");
+        }, 1000);
+        const data =  await  fetch("https://api.github.com/users/akshay");
+        const json = await data.json();
+        console.log(json);
+        this.setState({
+            userInfo : json,
+        });
+
     }
-    render() {
-        const {name , location} = this.props;   
+    componentDidUpdate(){
+        console.log("For every subsequent render it is called");
+        console.log("to behave as only once callled like when we used dependency array [] in useEffect in functional component we can use condition inside componentDidUpdate");
+        console.log("Example: if(this.state.count !== prevState.count) { // do something }");
+        if(this.state.count !== this.prevState?.count){
+            //do something when count changes then only this block will be executed
+        }
+    }
+    componentWillUnmount(){
+        console.log("Executed before the component is removed from the DOM" );
+        //used for cleanup activities
+        //like clearing timers, cancelling network requests or removing event listeners
+        clearInterval(this.timer);
+    } 
+    render() {  
         const {count , count2} = this.state;
+        const {name , location , avatar_url} = this.state.userInfo;
         return <div className="user-card">
         <h1>Count = {count}</h1>
         <h1>Count2 = {count2}</h1>
@@ -21,6 +51,7 @@ class UserClass extends React.Component {
                 count2 : count2 + 1
             })
         }}>Increment Count</button>
+        <img src={avatar_url} />
         <h2>Name:{name}</h2>
         <h2>Location:{location}</h2>
         <h3>Contact:bhavyanov5@</h3>
