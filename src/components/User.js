de08d1e14a85b048b7877fd06aa59e63f1e7c598
bhavya sree <bhavyasrigurram4@gmail.com>
2025-12-14 22:11:42 +0530

    this.setState for change of state variables

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
 const User = ({ name }) => {
-    const [count] = useState(0);
+    const [count , setCount] = useState(0);
     const [count2] = useState(1);
     return <div className="user-card">
         <h1>Count = {count}</h1>
         <h1>Count2 = {count2}</h1>
+        <button onClick={ () => {
+             setCount(count + 1);
+        }}>Increment Count</button>
         <h2>Name:{name}</h2>
         <h2>Location:PapayaPalem</h2>
         <h3>Contact:bhavyanov5@</h3>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -15,6 +15,12 @@ class UserClass extends React.Component {
         return <div className="user-card">
         <h1>Count = {count}</h1>
         <h1>Count2 = {count2}</h1>
+        <button onClick={ () => {
+            this.setState({
+                count : this.state.count + 1,
+                count2 : count2 + 1
+            })
+        }}>Increment Count</button>
         <h2>Name:{name}</h2>
         <h2>Location:{location}</h2>
         <h3>Contact:bhavyanov5@</h3>
